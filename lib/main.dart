dadcab78423f6a3568fff0f182bd5d9d932b72e0
vijuprashanth1990 <vijuprashanth1990@gmail.com>
2026-08-11 09:58:26 +0530

    comment oauth

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class PaymentListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sri Vignesh Chit',
+      title: 'Sri Vignesh Chits',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: SearchPage(),
     );
